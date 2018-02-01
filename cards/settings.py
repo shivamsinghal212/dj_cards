d9b22a8d8f9501903b6482e5e712aeb492cdb5cc
shivam <shivam.singhal212@gmail.com>
2018-02-01 20:38:17 +0530

    #ID-02 Added templates and basic configuration.

--- cards/settings.py
+++ cards/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
